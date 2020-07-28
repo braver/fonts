57660d4a349d9c8c12c6d1cd7bbf1a5801f3de5a
Nikolay Yakimov <root@livid.pp.ru>
2020-07-28 09:23:36 +0300

    Update fantasque-sans to v1.8.0

---
+++ resources/fantasque-sans/update.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+extract() {
+  curfiles=()
+  for i in *.woff2; do
+    curfiles+=("Webfonts/$(basename $i)")
+  done
+  7z -y e -o"." "$file" "${curfiles[@]}"
+}
+
+repo="belluzj/fantasque-sans"
+
+filter='match(".*-Normal\\.zip"; "i").string'
+source "../../scripts/update-github.sh"
+
+extract
+
+# no loop k
+
+filter='match(".*mono-NoLoopK\\.zip"; "i").string'
+source "../../scripts/update-github.sh"
+
+pushd no-loop-k
+extract
+popd
+
+echo "$tag" > ".tag"
+
+source "../../scripts/update-commit.sh"
